2b193a9f105f4ca2a41c790c5291e931e95d85a2
walter.c <walter.cepeda.thian@gmail.com>
2024-12-07 16:25:10 -0300

    arreglos mov

---
+++ JS/prueba.js
@@ -0,0 +1,18 @@
+let persona ={
+    nombre: "camila",
+    apellido: "torres",    
+    edad: 28,
+
+}
+
+switch(persona.nombre){
+
+    case "camila":
+        console.log(persona);
+        break;
+    case "pedro":
+        console.log(persona);
+        break;
+    default:
+        console.log("no se encontro");
+}
